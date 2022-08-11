094e3e04eddcd777c15141b2a8dd74ef25aa7461
Patrick Fournier <p_fournier@hushmail.com>
2022-08-11 12:18:40 -0400

    Use Ipython as python shell.

--- config/python.el
+++ config/python.el
@@ -8,7 +8,11 @@
   (python-mode . lsp)
   (python-mode . (lambda ()
                    (flycheck-select-checker 'python-flake8)
-                   (setq flycheck-flake8rc "~/.config/flake8")))
+                   (setq flycheck-flake8rc "~/.config/flake8"
+                         python-shell-interpreter "ipython"
+                         python-shell-interpreter-args "-i --simple-prompt --InteractiveShell.display_page=True"
+                         python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+                         python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: ")))
   (lsp-mode . (lambda () (setq lsp-diagnostic-package :none))))
 
 ;;; python.el ends here
